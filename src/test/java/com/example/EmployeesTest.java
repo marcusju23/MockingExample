ff56ca905d48891418f6afcd30b8cf3f6cfded05
marcusju23 <143085217+marcusju23@users.noreply.github.com>
2024-02-06 18:35:54 +0100

    Add test to check if successful when correct amount of payments

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -1,4 +1,28 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EmployeesTest {
+
+    private BankService bankService;
+    private EmployeeRepository employeeRepository;
+    private Employees employees;
+
+    @BeforeEach
+    void setUp(){
+        bankService = new BankServiceStub();
+        employeeRepository = new EmployeeRepositoryStub();
+        employees = new Employees(employeeRepository, bankService);
+    }
+
+    @Test
+    @DisplayName("should return successful when correct amount of payments")
+    void shouldReturnSuccessfulWhenCorrectAmountOfPayments() {
+        int payment = employees.payEmployees();
+        assertEquals(3, payment);
+    }
 }
