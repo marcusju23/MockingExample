0b9426bf63003613d43022e8c0172dac97d354cd
Marcus <marcus.olsson98@gmail.com>
2024-02-07 18:49:49 +0100

    Add Mock-test, should return unsuccessful when there are Zero employees

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -3,26 +3,45 @@ package com.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EmployeesTest {
 
+    @Mock
+    private EmployeeRepository employeeRepositoryMock;
+    @Mock
+    private BankService bankServiceMock;
+
     private BankService bankService;
     private EmployeeRepository employeeRepository;
-    private Employees employees;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         bankService = new BankServiceStub();
         employeeRepository = new EmployeeRepositoryStub();
-        employees = new Employees(employeeRepository, bankService);
     }
 
     @Test
     @DisplayName("should return successful when correct amount of payments")
     void shouldReturnSuccessfulWhenCorrectAmountOfPayments() {
+        Employees employees = new Employees(employeeRepository, bankService);
         int payment = employees.payEmployees();
         assertEquals(3, payment);
+    }
+
+    @Test
+    @DisplayName("should return unsuccessful when there are Zero employees")
+    void shouldReturnUnsuccessfulWhenThereAreZeroEmployees() {
+        MockitoAnnotations.openMocks(this);
+        Employees employees = new Employees(employeeRepositoryMock, bankServiceMock);
+        Mockito.when(employeeRepositoryMock.findAll()).thenReturn(Collections.emptyList());
+        int payment = employees.payEmployees();
+        assertEquals(0, payment);
     }
 }
