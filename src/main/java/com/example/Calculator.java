55204c60e35b91465635319ca05c4d72dec4804d
Marcus <marcus.olsson98@gmail.com>
2024-02-07 20:46:00 +0100

    Add method add

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -2,4 +2,18 @@ package com.example;
 
 public class Calculator {
 
+    public static int add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        String[] numberSegments = numbers.split(",");
+
+        int sum = 0;
+        for (String num : numberSegments) {
+            sum += Integer.parseInt(num);
+        }
+
+        return sum;
+    }
 }
